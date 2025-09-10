283070c24648923fa2b5969fd4cd51fbb0339ff4
md ismail <mdismail85773@gmail.com>
2025-09-10 21:49:18 +0800

    class deffin

--- destructuring.js
+++ destructuring.js
@@ -66,9 +66,69 @@ const actor ={name :'Tom Cruise', age: 58, country: 'USA'};
 
 
 // forEach function to get the square of an array and no return a new array
-    const numbers = [1,2,3,4,5,6,7,8,9];
+    // const numbers = [1,2,3,4,5,6,7,8,9];
 
-    const double = numbers.forEach( x => console.log(x * x));
-       console.log(double);
+    // const double = numbers.forEach( x => console.log(x * x));
+    //    console.log(double);
 
     
+
+// filter function to get the even numbers from an array and return a new array
+// const numbers = [1,2,3,4,5,6,7,8,9];
+//   const even = numbers.filter(n => n % 2 == 0);
+//     console.log(even);
+ 
+
+
+// firter function to get the names with even number of characters and provied a new array
+// const friends = ['TomHanks','TomBrady','TomCruise', 'TomSolaiman'];
+
+// const evenFriends = friends.filter(f => f.length % 2 == 0);
+// console.log(evenFriends);
+
+
+
+// find function to get the first name with even number of characters but not provide a new array
+// const friends = ['TomHanks','TomBrady','TomCruise', 'TomSolaiman'];
+
+// const evenFriends = friends.find(f => f.length % 2 == 0);
+// console.log(evenFriends);
+
+
+
+// class to create a person object and set the name, age and country in the constructor
+
+
+// class Teacher{
+
+//      constructor( name ,qualification, subject){
+//         this.name = name;
+//         this.qualification = qualification;
+//         this.subject = subject;
+//         }
+
+//     lecture(){
+//         console.log('I am a teacher')
+//     }
+// }
+
+// const teacher = new Teacher( 'Ismail','MSc', 'Math');
+
+// console.log(teacher);
+
+
+
+class car{
+    constructor( name, model, color){
+        this .name = name ;
+        this.model =model;
+        this.color = color;
+    }
+    move(){
+        console.log('I am moving');
+    }
+}
+
+const myCar = new car('Toyata',2023, 'Black');
+console.log(myCar);
+myCar.move();
